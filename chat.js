ad3d4bdad6b72c9613891788b767c15c4c68b3bf
klevtt <erenboluk@hotmail.com>
2023-05-14 10:51:32 +0300

    New info messages have been added. User participation information is given as info.

--- chat.js
+++ chat.js
@@ -1,13 +1,20 @@
-var chatBox = document.getElementById("chat-box");
+const chatBox = document.getElementById("chat-box");
+const chatContainer = document.getElementById("chat-container");
+const connText = document.getElementById("conn-text");
+
+window.onload = function () {
+    scrollChatToBottom();
+};
 
 var conn = new WebSocket('ws://localhost:8080');
 
 conn.onopen = function (e) {
-    console.log("Connection established!");
+    connText.remove();
+    chatContainer.append(prepareInfoText("[ Connection Success ] Welcome to the chat!"));
     console.log(e.data);
 };
 conn.onclose = function (e) {
-    console.log("Connection closed!");
+    chatContainer.append(prepareInfoText("[ Connection lost! ] There seems to be a system issue.!"));
 };
 conn.onmessage = function (e) {
     let data = JSON.parse(e.data);
@@ -19,14 +26,22 @@ conn.onmessage = function (e) {
     }
     if (data.type == "message") {
         console.log(data.data);
-        document.getElementById("chat-box").append(prepareMessage(data.data, data.from, true));
+        chatContainer.append(prepareMessage(data.data, data.from, true));
+        scrollChatToBottom();
+    }
+    if (data.type == "join") {
+        chatContainer.append(prepareInfoText(` user ${data.id} joinded chat !`));
+        scrollChatToBottom();
     }
 };
 
 $('#msg').submit(function (e) {
     e.preventDefault();
     var msg = document.getElementById("message-box").value;
-    document.getElementById("chat-box").append(prepareMessage(msg, 'me', false));
+    if (msg == "") return;
+    chatContainer.append(prepareMessage(msg, 'me', false));
+    scrollChatToBottom();
+    $(this).trigger('reset');
     conn.send(msg);
 });
 
@@ -41,6 +56,19 @@ function prepareMessage(message, author, type) {
     msgText.className = "msg-text";
     msgText.innerText = message;
     msgDiv.appendChild(msgAuthor);
+    msgDiv.appendChild(msgText);
+    return msgDiv;
+}
+function scrollChatToBottom() {
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+function prepareInfoText(message) {
+    let msgDiv = document.createElement("div");
+    msgDiv.className = "msg-info";
+    let msgText = document.createElement("span");
+    msgText.className = "msg-info-text";
+    msgText.innerText = message;
     msgDiv.appendChild(msgText);
     return msgDiv;
 }
